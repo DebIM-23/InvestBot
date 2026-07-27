4210359eca1092bb0d9ba475c6fb9dd6bacbb144
DEBDEEP DAN <debdeepdan@gmail.com>
2026-07-27 19:55:41 +0530

    Simplify start.sh by combining commands

--- backend/start.sh
+++ backend/start.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-# Start Python FastAPI in background
-cd /app/python
-python3 -m uvicorn data_fetcher:app --host 0.0.0.0 --port 8000 &
-PYTHON_PID=$!
-
-# Wait a moment for Python to start
+cd /app/python && python3 -m uvicorn data_fetcher:app --host 0.0.0.0 --port 8000 &
 sleep 3
-
-# Start Node.js API
-cd /app
-node init-db.js
-node server.js
-
-# If Node crashes, kill Python too
-kill $PYTHON_PID
+cd /app && node init-db.js && node server.js
